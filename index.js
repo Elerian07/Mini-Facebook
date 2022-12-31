229233ca215ab1f74d74f36dcddd696862212d02
Mohamed Elerian <103853528+Elerian07@users.noreply.github.com>
2022-12-31 21:39:01 +0200

    f

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ import connection from './DB/connection.js';
 import allRoutes from './module/index.route.js';
 const app = express();
 app.use(express.json());
-
+app.get("/", (req, res) => {
+    res.send("<h1>Home Page</h1 > <br> <h5>By Mohamed Elerian</h5>")
+})
 app.use(`${process.env.baseUrl}user`, allRoutes.userRoute)
 app.use(`${process.env.baseUrl}post`, allRoutes.postRoute)
 app.use(`${process.env.baseUrl}auth`, allRoutes.authRoute)
